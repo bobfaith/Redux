871e2e399efba79a9c16d27867a333a8fe97d449
bobfaith <chettri.babin@gmail.com>
2021-09-27 17:11:53 +0530

    starting with redux

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <h3 className="my-3">Deposit/Withdraw Money</h3>
+            <button className="btn-primary mx-3">-</button>
+            Update Balance
+            <button className="btn-primary mx-3">+</button>
+        </div>
+    )
+}
+
+export default Shop
