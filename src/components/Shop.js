dc7603b60b4442e53ad6ec6fcda41ece61f3e735
bobfaith <chettri.babin@gmail.com>
2021-10-07 18:21:20 +0530

    "Without using Bind Action Creater being used in Shopjs"

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state/index'
 
 const Shop = () => {
+    const dispatch = useDispatch();
     return (
         <div>
             <h3 className="my-3">Deposit/Withdraw Money</h3>
-            <button className="btn-primary mx-3">-</button>
+            <button className="btn-primary mx-3" onClick={() => {dispatch(actionCreators.withdrawMoney(100))}}>-</button>
             Update Balance
-            <button className="btn-primary mx-3">+</button>
+            <button className="btn-primary mx-3" onClick={() => {dispatch(actionCreators.depositMoney(100))}}>+</button>
         </div>
     )
 }
